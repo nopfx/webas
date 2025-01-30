21d58c380e21e68066d2b6615355e02b86adcbd8
Lazy Monad <lazymonad@Lazys-MacBook-Air.local>
2025-01-30 09:31:55 +0200

    From markdown generated HTML pass over Twig templating

--- src/blog.rs
+++ src/blog.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::{Result, Write};
 use std::path::Path;
 
+use tera::{Context, Error, Tera};
+
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub struct Post {
     pub title: String,
@@ -23,8 +25,25 @@ impl Post {
     }
     pub fn create(&self, location: String) -> Result<()> {
         let loc = Path::new(&location).join(&self.slug);
-        let mut file = File::create(loc)?;
-        file.write_all(self.text.as_bytes())?;
+        let mut file = File::create(&loc)?;
+
+        let mut html = String::from("{% extends \"parts/base.twig\" %}");
+        let title =
+            String::from("{% block title %}") + self.title.as_str() + "{% endblock title %}";
+        let body = String::from("{% block body %}") + self.text.as_str() + "{% endblock body %}";
+
+        html.push_str(&title);
+        html.push_str(&body);
+
+        let mut tera = Tera::new("./tests/template/pages/**/*").unwrap();
+        tera.add_raw_template(&loc.to_str().unwrap(), html.as_str());
+
+        let output = tera
+            .render(&loc.to_str().unwrap(), &Context::new())
+            .unwrap();
+
+        file.write_all(output.as_bytes())?;
+
         Ok(())
     }
 }
