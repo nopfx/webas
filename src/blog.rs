779cd56143af8de1938fcc879e764d0195a5153b
nop.f(x) <nop.fx@nopfxs-MacBook-Air.local>
2025-02-04 09:21:34 +0200

    Improve main.rs file

--- src/blog.rs
+++ src/blog.rs
@@ -24,19 +24,21 @@ impl Post {
         }
     }
     pub fn create(&self, location: String) -> Result<()> {
-        let loc = Path::new(&location).join(&self.slug);
-        let mut file = File::create(&loc)?;
+        let loc = format!("{}/{}", &location, &self.slug);
+
+        let loc = Path::new(&loc);
+        let mut file = File::create(&loc).unwrap();
 
         let mut html = String::from("{% extends \"parts/base.twig\" %}");
-        let title =
-            String::from("{% block title %}") + self.title.as_str() + "{% endblock title %}";
-        let body = String::from("{% block body %}") + self.text.as_str() + "{% endblock body %}";
+        //let title =
+        //    String::from("{% block title %}") + self.title.as_str() + "{% endblock title %}";
+        let body = String::from("{% block main %}") + self.text.as_str() + "{% endblock main %}";
 
-        html.push_str(&title);
+        //html.push_str(&title);
         html.push_str(&body);
 
         let mut tera = Tera::new("./tests/template/pages/**/*").unwrap();
-        tera.add_raw_template(&loc.to_str().unwrap(), html.as_str());
+        let _ = tera.add_raw_template(&loc.to_str().unwrap(), html.as_str());
 
         let output = tera
             .render(&loc.to_str().unwrap(), &Context::new())
--- src/main.rs
+++ src/main.rs
@@ -16,42 +16,53 @@ use std::path::Path;
 use tera::Context;
 
 fn main() {
-    if let Some(posts_location) = cli::get::<String>("posts".into()) {
-        if let Some(pages_location) = cli::get::<String>("pages".into()) {
-            if Path::new("./tests/web").exists() {
-                fs::remove_dir_all("./tests/web").unwrap();
-            }
-            fs::create_dir("./tests/web");
-            let options = CopyOptions::new().copy_inside(true);
-            let src_folder = Path::new("./tests/template/assets");
-            let dst_folder = Path::new("./tests/web/assets");
-            copy(src_folder, dst_folder, &options).unwrap();
+    // tests/template/posts/
+    // --template tests/template
+    let template_location = cli::get::<String>("template".into()).unwrap_or("".into());
+    // --web ./tests/web
+    let web_location = cli::get::<String>("web".into()).unwrap_or("./".into());
 
-            let posts: Vec<Post> = loaders::get_files_by_type(posts_location, "md".into())
-                .into_iter()
-                .map(|f| Markdown::new(f))
-                .map(|m| m.to_post().unwrap())
-                .collect();
+    // TODO:
+    // make checks for required CLI params
+    if Path::new(&web_location).exists() {
+        fs::remove_dir_all(&web_location).unwrap();
+    }
 
-            let templates: Vec<Twig> = loaders::get_files_by_type(pages_location, "twig".into())
-                .into_iter()
-                .map(|f| Twig::new(f))
-                .collect();
+    let _ = fs::create_dir(&web_location);
+    let options = CopyOptions::new().copy_inside(true);
+    let src_location = format!("{}/{}", &template_location, "/assets");
+    let src_folder = Path::new(&src_location);
+    let dst_location = format!("{}/{}", &web_location, "/assets");
+    let dst_folder = Path::new(&dst_location);
+    copy(src_folder, dst_folder, &options).unwrap();
 
-            for template in templates {
-                let mut context = Context::new();
-                context.insert("posts", &posts);
-                let path = Path::new(template.file.location.as_str());
-                let file_stem: &str = path.file_stem().unwrap().to_str().unwrap();
+    let posts: Vec<Post> =
+        loaders::get_files_by_type(format!("{}/{}", &template_location, "/posts"), "md".into())
+            .into_iter()
+            .map(|f| Markdown::new(f))
+            .map(|m| m.to_post().unwrap())
+            .collect();
 
-                let mut file =
-                    File::create(String::from("./tests/web/") + file_stem + ".html").unwrap();
-                file.write_all(template.to_html(context).unwrap().as_bytes());
+    let templates: Vec<Twig> = loaders::get_files_by_type(
+        format!("{}/{}", &template_location, "/pages"),
+        "twig".into(),
+    )
+    .into_iter()
+    .map(|f| Twig::new(f))
+    .collect();
 
-                for post in &posts {
-                    post.create("tests/web/".into());
-                }
-            }
-        }
+    for template in templates {
+        let mut context = Context::new();
+        context.insert("posts", &posts);
+        let path = Path::new(template.file.location.as_str());
+        let file_stem: &str = path.file_stem().unwrap().to_str().unwrap();
+
+        let file_location = format!("{}/{}.{}", &web_location, file_stem, "html");
+        let mut file = File::create(&file_location).unwrap();
+        let _ = file.write_all(template.to_html(context).unwrap().as_bytes());
+    }
+
+    for post in &posts {
+        let _ = post.create(String::from(&web_location));
     }
 }
